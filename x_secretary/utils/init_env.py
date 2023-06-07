b038bc0788379bd1436c61cf0bf3686a5ae5e0f3
x99 <xhr@x99>
2023-06-08 00:27:23 +0800

    tf32

--- x_secretary/utils/init_env.py
+++ x_secretary/utils/init_env.py
@@ -13,6 +13,14 @@ def init_cuda(cuda_devices,ddp=False,tf32=False):
     os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
     if tf32 is not True:
         os.environ['NVIDIA_TF32_OVERRIDE']='0'
+        # The flag below controls whether to allow TF32 on matmul. This flag defaults to False in PyTorch 1.12 and later.
+        torch.backends.cuda.matmul.allow_tf32 = False
+        # The flag below controls whether to allow TF32 on cuDNN. This flag defaults to True.
+        torch.backends.cudnn.allow_tf32 = False
+        
+    else:
+        torch.backends.cuda.matmul.allow_tf32 = True
+        torch.backends.cudnn.allow_tf32 = True
 
     if(ddp):
         distributed.init_process_group(backend='nccl')
