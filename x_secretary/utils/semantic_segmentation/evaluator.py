9a24c9be72ac884d17170b459d6815f860e95066
x99 <xhr@x99>
2023-09-11 12:24:18 +0800

    debug

--- x_secretary/utils/semantic_segmentation/evaluator.py
+++ x_secretary/utils/semantic_segmentation/evaluator.py
@@ -4,6 +4,7 @@ import cv2
 from .metric import Metric
 from pathlib import Path
 import numpy as np
+import einops
 class EvaluatorBase:
     def __init__(self, model:torch.nn.Module,n_classes,color_table, process_image:callable):
         '''
@@ -23,7 +24,61 @@ class EvaluatorBase:
         self.n_classes=n_classes
         self.color_table=color_table
         self.process_image=process_image
-    
+
+    def get_pred(self,img:torch.Tensor) -> torch.Tensor:
+        """
+        predict one image
+
+        Args:
+            img (torch.Tensor): _description_
+
+        Returns:
+            torch.Tensor: _description_
+        """
+        with torch.no_grad():
+            inputs = torch.unsqueeze(img, 0) # [b c h w]
+            output = self.model(inputs).cpu()
+            b, c, h, w = output.shape
+            assert b == 1 and self.n_classes==c
+            pred= einops.rearrange(output,'b c h w -> h w (b c)')
+            pred = pred.argmax(dim=2,keepdim=False) # [h w c] -> [h w]
+
+        return pred
+
+    def get_masked_results(self,img:torch.Tensor) -> np.ndarray:
+        """
+        predict one image and render the results in color
+
+        Args:
+            img (np.ndarray): original image (pre-processed)
+
+        Returns:
+            results picture, (BGR)
+        """
+        o_c ,o_h, o_w = img.shape
+
+        pred=self.get_pred(img).numpy()
+        pred_img = np.zeros((h, w, 3), dtype=np.float32)
+        for cls in range(self.n_classes):
+            pred_inds = pred == cls
+            # label = index2label[cls]
+            color=self.color_table[cls]
+            pred_img[pred_inds] = color
+
+        pred_img = cv2.resize(pred_img,(o_w,o_h))
+        return pred_img
+
+    def test_img(self,img_path,out_path):
+        '''
+        test one img, and save colored mask to file
+        '''
+        img,origin_shape= self.process_image(str(img_path))
+
+        pred_img = self.get_masked_results(img)
+        pred_img = cv2.cvtColor(pred_img,cv2.COLOR_RGB2BGR)
+
+        cv2.imwrite(str(out_path),pred_img)
+
     def test_folder(self,img_dir,folder='origin',destination_folder='masked',finish_hook:callable=None):
         '''
         test multiple images, saving masked image to img_dir/masked/
@@ -47,51 +102,7 @@ class EvaluatorBase:
 
         for f,r in files:
             self.test_img(f,r)
-            if finish_hook is not None:
-                finish_hook(f)
-
-    def test_img(self,img_path,out_path):
-        '''
-        test one img, and save colored mask to file
-        '''
-        img,origin_shape= self.process_image(str(img_path))
-
-        pred_img = self.get_masked_results(img)
-        pred_img = cv2.cvtColor(pred_img,cv2.COLOR_RGB2BGR)
-
-        cv2.imwrite(str(out_path),pred_img)
-
-    def get_masked_results(self,img:torch.Tensor) -> np.ndarray:
-        """
-        predict one image and render the results in color
-
-        Args:
-            img (np.ndarray): original image (pre-processed)
-
-        Returns:
-            results picture, (BGR)
-        """
-        o_c ,o_h, o_w = img.shape
-
-        inputs = torch.unsqueeze(img, 0) # [b,c,h,w]
-        with torch.no_grad():
-            output = self.model(inputs)
-
-        output = output.data.cpu().numpy()
-
-        N, _, h, w = output.shape
-        assert (N == 1)
-        pred = output.transpose(0, 2, 3, 1).reshape(-1, self.n_classes).argmax(axis=1).reshape(h, w)
-
-        pred_img = np.zeros((h, w, 3), dtype=np.float32)
-        for cls in range(self.n_classes):
-            pred_inds = pred == cls
-            # label = index2label[cls]
-            color=self.color_table[cls]
-            pred_img[pred_inds] = color
-
-        pred_img = cv2.resize(pred_img,(o_w,o_h))
-        return pred_img
+            if finish_hook is not None: finish_hook(f)
 
     def calculate_metrics(self,pred,gt):
         metric=Metric(self.n_classes)
