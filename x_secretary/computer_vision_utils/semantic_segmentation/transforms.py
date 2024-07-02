bd51542afbe93555eb592b52449d55371d7bc5e7
msk <msk@ccnt_amax>
2024-07-02 10:11:36 +0800

    adapt to shape for seg

--- x_secretary/computer_vision_utils/semantic_segmentation/transforms.py
+++ x_secretary/computer_vision_utils/semantic_segmentation/transforms.py
@@ -2,6 +2,7 @@ import torch,numpy
 from torchvision import transforms
 import cv2 
 import numpy as np
+import einops
 def flip_segmentation(img,label,p):
     '''
     Horizonal Flip at possibility P
@@ -39,3 +40,40 @@ def opencv_seg_label_to_torchTensor(label: numpy.ndarray) -> torch.Tensor:
     # [h,w,c] -> [c,h,w] -> [h,w]
     label=label.permute(2,0,1)[0]
     return label
+
+# RGB
+MEAN=[0.485,0.456,0.406]
+def adapt_to_shape(img:np.ndarray,label:np.ndarray,target_shape:tuple,ignore_label=255):
+    _oh,_ow,_oc=img.shape
+    _dh=abs(_oh-target_shape[0])
+    _dw=abs(_ow-target_shape[1])
+
+    _rslt_img=(np.array(MEAN[::-1])*255).astype(img.dtype) # mean of ImageNet-1k, bgr
+    _rslt_img=einops.repeat(_rslt_img,'c -> h w c', h=target_shape[0],w=target_shape[1])
+    _rslt_label=np.zeros_like(_rslt_img,dtype=label.dtype)+255
+
+
+    if _dh > _dw : # resize the image in the w dimension
+        _new_w= target_shape[1]
+        _new_h = int(_oh / _ow * _new_w)
+        _new_h = min(target_shape[0],_new_h)
+
+    else: # resize the image in the h dimension
+        _new_h = target_shape[0]
+        _new_w = int(_ow / _oh * _new_h)
+        _new_w = min(target_shape[1],_new_w)
+
+    _source_img=cv2.resize(img,(_new_w,_new_h))
+    _source_label=cv2.resize(label,(_new_w,_new_h),interpolation=cv2.INTER_NEAREST)
+
+    _start_x=int((target_shape[1]-_source_img.shape[1])/2)
+    _start_y=int((target_shape[0]-_source_img.shape[0])/2)
+
+    _rslt_img[
+        _start_y:_start_y+_source_img.shape[0],
+        _start_x:_start_x+_source_img.shape[1],:]=_source_img
+    _rslt_label[
+        _start_y:_start_y+_source_label.shape[0],
+        _start_x:_start_x+_source_label.shape[1],:]=_source_label
+    
+    return _rslt_img,_rslt_label
