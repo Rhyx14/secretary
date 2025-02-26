fffafa08adb2911232baa47dc0f4d179fd623ab8
x99 <xhr@x99>
2025-02-26 15:03:15 +0800

    bug fix

--- x_secretary/utils/split_decay_parameters.py
+++ x_secretary/utils/split_decay_parameters.py
@@ -9,18 +9,18 @@ def split_decay_parameters(module:torch.nn.Module) -> tuple:
     params_decay = []
     params_no_decay = []
 
-    if isinstance(_m, torch.nn.Linear):
-        params_decay.append(_m.weight)
-        if _m.bias is not None:
-                params_no_decay.append(_m.bias)
+    if isinstance(module, torch.nn.Linear):
+        params_decay.append(module.weight)
+        if module.bias is not None:
+            params_no_decay.append(module.bias)
 
-    elif isinstance(_m, torch.nn.modules.conv._ConvNd):
-        params_decay.append(_m.weight)
-        if _m.bias is not None:
-            params_no_decay.append(_m.bias)
+    elif isinstance(module, torch.nn.modules.conv._ConvNd):
+        params_decay.append(module.weight)
+        if module.bias is not None:
+            params_no_decay.append(module.bias)
 
-    elif isinstance(_m, (torch.nn.modules.batchnorm._BatchNorm,)):
-        params_no_decay.extend([*_m.parameters()])
+    elif isinstance(module, (torch.nn.modules.batchnorm._BatchNorm,)):
+        params_no_decay.extend([*module.parameters()])
 
     elif hasattr(module,'split_decay_parameters'):
         _decay,_no_decay=module.split_decay_parameters()
@@ -28,10 +28,10 @@ def split_decay_parameters(module:torch.nn.Module) -> tuple:
         params_no_decay.extend([*_no_decay])
 
     else: # for default settings, no decay for immediate parameters. Then recurs its sub-module.
-        for _2_param in _m._parameters.values():
-            params_no_decay.append(_2_param)
+        for _param in module._parameters.values():
+            params_no_decay.append(_param)
         for _m in module.children():
-            _decay,_no_decay=split_parameters_for_decay(_m)
+            _decay,_no_decay=split_decay_parameters(_m)
             params_decay.extend([*_decay])
             params_no_decay.extend([*_no_decay])
     
