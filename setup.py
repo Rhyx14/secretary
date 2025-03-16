cfe9335c3d2a978df46d15c66d15abb36a3ab57d
x99 <xhr@x99>
2025-03-16 19:09:38 +0800

    xs-draw

--- setup.py
+++ setup.py
@@ -1,30 +1,42 @@
 from setuptools import setup, find_packages
 from os import path
+
 this_directory = path.abspath(path.dirname(__file__))
-long_description = None
-# with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-#     long_description = f.read()
- 
+
+# Read the long description from README.md
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
-      name='x_secretary', # 包名称
-      packages=find_packages(exclude=['__pycache__']), # 需要处理的包目录
-      version='0.3.250228', # 版本
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python', 'Intended Audience :: Developers',
-          'Programming Language :: Python :: 3.10',
-      ],
-      install_requires=['torch>=2.0.1','opencv-python','tqdm','pyyaml','accelerate>=1.1.0','torchvision>=0.15','pytorch_warmup','loguru'],
-      scripts=['bin/xsrun'],
-    #   entry_points={'console_scripts': ['pmm=pimm.pimm_module:main']},
-    #   package_data={'': ['*.json']},
-      author='rhyx14', # 作者
-      description='Auxiliary tools for pytorch training.', # 介绍
-    #   long_description=long_description, # 长介绍，在pypi项目页显示
-    #   long_description_content_type='text/markdown', # 长介绍使用的类型
-      url='https://github.com/Rhyx14/secretary', # 包主页，一般是github项目主页
-      license='MIT', # 协议
-      python_requires='>=3.10'
-    #   keywords='pimm source manager'  # 关键字 搜索用
+    name='x_secretary',  # Package name
+    packages=find_packages(exclude=['__pycache__']),  # Include all packages except __pycache__
+    version='0.3.250316',  # Version
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.10',
+    ],
+    install_requires=[
+        'torch>=2.0.1',
+        'opencv-python',
+        'tqdm',
+        'pyyaml',
+        'accelerate>=1.1.0',
+        'torchvision>=0.15',
+        'pytorch_warmup',
+        'loguru',
+    ],
+    scripts=['bin/xsrun', 'bin/xs-draw'],  # Executable scripts
+    author='rhyx14',  # Author
+    description='Auxiliary tools for pytorch training.',  # Short description
+    long_description=long_description,  # Long description from README.md
+    long_description_content_type='text/markdown',  # Format of the long description
+    url='https://github.com/Rhyx14/secretary',  # Project homepage
+    license='MIT',  # License
+    python_requires='>=3.10',  # Python version requirement
+    keywords='pytorch training tools machine-learning deep-learning',  # Keywords
+    # package_data={'': ['*.json']},  # Include non-Python files if needed
+    # entry_points={'console_scripts': ['x_secretary=x_secretary.cli:main']},  # Console scripts
 )
