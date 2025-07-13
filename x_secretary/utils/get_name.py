affddc248c13fea818ccf72014a3e6af5294b6f2
x99 <xhr@x99>
2025-07-13 11:20:31 +0800

    get_name support functools.partial

--- x_secretary/utils/get_name.py
+++ x_secretary/utils/get_name.py
@@ -1,7 +1,8 @@
 from typing import Any
 import torch
 from torch.nn.parallel.distributed import DistributedDataParallel
-def get_name(obj:torch.nn.Module | DistributedDataParallel | Any) -> str:
+from functools import partial
+def get_name(obj:torch.nn.Module | DistributedDataParallel | partial | Any) -> str:
     '''
     Get the name str of an object.
 
@@ -11,6 +12,8 @@ def get_name(obj:torch.nn.Module | DistributedDataParallel | Any) -> str:
     '''
     if isinstance(obj,DistributedDataParallel):
         obj=obj.module
+    elif isinstance(obj,partial):
+        obj=obj.func
     if hasattr(obj,'name'):
         return obj.name
     else:
