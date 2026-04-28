6e2993f3d69632ed2fe8bdf3671b17dbc40cb79e
msk <msk@h12d>
2026-04-28 11:32:28 +0800

    remove set seed

--- x_secretary/__init__.py
+++ x_secretary/__init__.py
@@ -10,7 +10,6 @@ from .data_recorder import Serial,Avg,DataRecorder
 from .utils.ddp_sampler import DDP_BatchSampler
 from .utils.sys_info import get_sys_info,get_host_name
 from .utils.init_env import init_cuda
-from .utils.set_seeds import set_seed
 from .utils.time import get_str_time,measure_func_time,measure_time
 from .utils.log_dir import Log_dir
 from .utils.opencv_loader import OpenCV_Loader
@@ -28,7 +27,6 @@ __all__.extend([
     'Serial','Avg','DataRecorder',
     'DDP_BatchSampler',
     'init_cuda',
-    'set_seed',
     'get_sys_info',
     'get_host_name',
     'get_str_time',"measure_func_time","measure_time",
--- x_secretary/utils/set_seeds.py
+++ x_secretary/utils/set_seeds.py
@@ -1,21 +1,7 @@
 import random
 import numpy as np
 import torch
-from ..deprecated import deprecated
-@deprecated('using accelerator.utils.set_seed instead')
-def set_seed(seed,determinstic=True):
-    '''
-    set random seed of torch, numpy and random
-    '''
-    torch.manual_seed(seed)
-    random.seed(seed)  # random
-    np.random.seed(seed)  # numpy
-    torch.manual_seed(seed)  # torch+CPU
-    torch.cuda.manual_seed(seed)  # torch+GPU
-    if determinstic:
-        torch.use_deterministic_algorithms(True,warn_only=True)
-        torch.backends.cudnn.benchmark = False
-        
+
 import random
 # https://pytorch.org/docs/stable/notes/randomness.html
 def seed_worker(worker_id):
